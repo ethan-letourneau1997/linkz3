6aa6e0bdf13579e827bb82781e0c6642ed969b9f
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-07 00:55:29 -0400

    Refactor post delete button into seperate component

---
+++ features/post/components/post-delete-button.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Button, Modal } from "flowbite-react";
+import { Post, PostRouterParams } from "@/types";
+
+import { AiOutlineDelete } from "react-icons/ai";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { deletePost } from "../api/delete-post";
+import { useState } from "react";
+
+type DeletePostProps = {
+  post: Post;
+  params: PostRouterParams;
+};
+
+export function PostDeleteButton({ post, params }: DeletePostProps) {
+  const [openModal, setOpenModal] = useState(false);
+
+  function handleDeletePost() {
+    deletePost(post, params.spaceName);
+    setOpenModal(false);
+  }
+
+  return (
+    <>
+      <button onClick={() => setOpenModal(true)}>
+        <AiOutlineDelete className="text-xl" />
+      </button>
+      <Modal
+        show={openModal}
+        size="md"
+        popup
+        onClose={() => setOpenModal(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 text-gray-400 h-14 w-14 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this post?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeletePost}>
+                Yes, I&apos;m sure
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
--- features/post/components/post-options.tsx
+++ features/post/components/post-options.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
-import { Button, Modal, Tooltip } from "flowbite-react";
 import { Post, PostRouterParams } from "@/types";
 
-import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { AiOutlineEdit } from "react-icons/ai";
 import Link from "next/link";
-import { deletePost } from "../api/delete-post";
-import { useState } from "react";
+import { PostDeleteButton } from "./post-delete-button";
+import { Tooltip } from "flowbite-react";
 
 type PostOptionsProps = {
   post: Post;
@@ -18,7 +16,7 @@ export function PostOptions({ post, params }: PostOptionsProps) {
   return (
     <div className="flex gap-2">
       <Tooltip content="Delete Post" style="dark">
-        <DeletePost post={post} params={params} />
+        <PostDeleteButton post={post} params={params} />
       </Tooltip>
       <Tooltip content="Edit Post" style="dark">
         <Link href={`${post.id}/edit`}>
@@ -26,51 +24,5 @@ export function PostOptions({ post, params }: PostOptionsProps) {
         </Link>
       </Tooltip>
     </div>
-  );
-}
-
-type DeletePostProps = {
-  post: Post;
-  params: PostRouterParams;
-};
-
-export function DeletePost({ post, params }: DeletePostProps) {
-  const [openModal, setOpenModal] = useState(false);
-
-  function handleDeletePost() {
-    deletePost(post, params.spaceName);
-    setOpenModal(false);
-  }
-
-  return (
-    <>
-      <button onClick={() => setOpenModal(true)}>
-        <AiOutlineDelete className="text-xl" />
-      </button>
-      <Modal
-        show={openModal}
-        size="md"
-        popup
-        onClose={() => setOpenModal(false)}
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 text-gray-400 h-14 w-14 dark:text-gray-200" />
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this post?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeletePost}>
-                Yes, I&apos;m sure
-              </Button>
-              <Button color="gray" onClick={() => setOpenModal(false)}>
-                No, cancel
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-    </>
   );
 }
