71779f2f177db00ff9ce34b663ba794998ea46d4
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-09 02:11:04 -0400

    Fix type errors

--- app/subscriptions/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SubscriptionsLayout } from "@/features/layout/subscriptions-layout";
-
-export const dynamic = "force-dynamic";
-
-export default async function Index() {
-  return <SubscriptionsLayout />;
-}
