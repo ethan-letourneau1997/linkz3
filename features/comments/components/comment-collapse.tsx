bcba4bac0dbe8f3bb5f8c86dcd68a33843fbcd80
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-14 18:55:08 -0400

    Create new component for mobile components

--- features/comments/components/comment-collapse.tsx
+++ features/comments/components/comment-collapse.tsx
@@ -21,34 +21,56 @@ export function CommentCollapse({ header, children }: CommentCollapseProps) {
     setOpen(false);
   }
 
-  return (
-    <div className="w-full max-w-3xl ">
-      <div className="flex gap-2 mt-5">
-        <button onClick={handleOpen} className={`${open ? "hidden" : "block"}`}>
-          <CgArrowsExpandLeft className="text-neutral-400" />
-        </button>
-        <button
-          onClick={handleClose}
-          className={`${open ? "block" : "hidden"} md:hidden`}
-        >
-          <MdCloseFullscreen className="rotate-90 text-neutral-400" />
-        </button>
-        {header}
-      </div>
+  function handleToggle() {
+    setOpen(!open);
+  }
 
-      <Collapsible open={open}>
-        <CollapsibleContent className="CollapsibleContent">
-          <div className="flex pt-2 pl-1">
-            <button
-              onClick={handleClose}
-              className="flex justify-center w-4 group"
-            >
-              <div className="bg-neutral-700 w-[1px] group-hover:bg-neutral-300" />
-            </button>
-            <div className="w-full md:pl-4">{children}</div>
-          </div>
-        </CollapsibleContent>
-      </Collapsible>
-    </div>
+  return (
+    <>
+      <div className="w-full max-w-3xl mt-4 ml-2 sm:hidden">
+        <button className="w-full" onClick={handleToggle}>
+          {header}
+        </button>
+
+        <Collapsible open={open}>
+          <CollapsibleContent className=" CollapsibleContent">
+            <div className="w-full px-2 ml-2 border-l border-neutral-800">
+              {children}
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+      </div>
+      <div className="hidden w-full max-w-3xl sm:block">
+        <div className="flex gap-2 mt-5">
+          <button
+            onClick={handleOpen}
+            className={`${open ? "hidden" : "block"}`}
+          >
+            <CgArrowsExpandLeft className="text-neutral-400" />
+          </button>
+          <button
+            onClick={handleClose}
+            className={`${open ? "block" : "hidden"} md:hidden`}
+          >
+            <MdCloseFullscreen className="rotate-90 text-neutral-400" />
+          </button>
+          {header}
+        </div>
+
+        <Collapsible open={open}>
+          <CollapsibleContent className="CollapsibleContent">
+            <div className="flex pt-2 pl-1">
+              <button
+                onClick={handleClose}
+                className="flex justify-center w-4 group"
+              >
+                <div className="bg-neutral-700 w-[1px] group-hover:bg-neutral-300" />
+              </button>
+              <div className="w-full md:pl-4">{children}</div>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+      </div>
+    </>
   );
 }
