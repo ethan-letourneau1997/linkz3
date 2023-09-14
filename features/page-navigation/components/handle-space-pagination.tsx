58930d082045072e23f3f2c07fa100341279691d
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-14 19:40:44 -0400

    Use swr for space pagination fetching

--- features/page-navigation/components/handle-space-pagination.tsx
+++ features/page-navigation/components/handle-space-pagination.tsx
@@ -1,36 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import { PageNavigation } from "./page-navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useParams } from "next/navigation";
+import useSWR from "swr";
 
 export function HandleSpacePagination() {
   const params = useParams();
 
   const activePage = parseInt(params.page as string, 10);
-  const supabase = createClientComponentClient();
-  const [pageCount, setPageCount] = useState(0);
 
-  async function getPostCount() {
+  const supabase = createClientComponentClient();
+
+  const { data: pageCount } = useSWR("post", async () => {
     const { count } = await supabase
       .from("post")
       .select("*", { count: "exact", head: true })
       .eq("posted_in", params.spaceId);
     if (count) {
       const pages = Math.ceil(count / 10);
-      setPageCount(pages);
+      return pages;
     }
-  }
+  });
 
-  useEffect(() => {
-    getPostCount();
-  }, [params]);
-
-  return (
-    <div className="flex justify-center mt-3">
-      <PageNavigation activePage={activePage} pageCount={pageCount} />
-    </div>
-  );
+  if (pageCount)
+    return (
+      <div className="flex justify-center mt-3">
+        <PageNavigation activePage={activePage} pageCount={pageCount} />
+      </div>
+    );
 }
