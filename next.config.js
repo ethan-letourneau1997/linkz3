4fb67f732df7288870b38564272ed1ddcb158ea2
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-14 22:09:33 -0400

    Replace link preview thumbnail background image with Nextjs Image component

--- next.config.js
+++ next.config.js
@@ -3,16 +3,23 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "mwzpdguznloapmfvlzxt.supabase.co",
-        port: "",
+        hostname: "**",
       },
     ],
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "mwzpdguznloapmfvlzxt.supabase.co",
+  //       port: "",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
