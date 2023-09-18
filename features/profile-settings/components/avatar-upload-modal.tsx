4e3490b45102612b52a1570556feac0accb662d7
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-18 19:22:31 -0400

    Set avatar modal to controlled

--- features/profile-settings/components/avatar-upload-modal.tsx
+++ features/profile-settings/components/avatar-upload-modal.tsx
@@ -8,23 +8,33 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { Button } from "@/components/ui/button";
 import { PublicProfile } from "@/types";
 import { UploadAvatar } from "./upload-avatar";
+import { useState } from "react";
 
 type AvatarUploadModalProps = {
   user: PublicProfile;
 };
 
 export function AvatarUploadModal({ user }: AvatarUploadModalProps) {
+  const [open, setOpen] = useState(false);
+
+  function closeModal() {
+    setOpen(false);
+  }
+
   return (
     <div>
-      <Dialog>
-        <DialogTrigger>Update Avatar</DialogTrigger>
-        <DialogContent className="h-screen max-w-3xl">
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Update Avatar</Button>
+        </DialogTrigger>
+        <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+            <DialogTitle>Select Avatar Image to Upload</DialogTitle>
 
-            <UploadAvatar user={user} />
+            <UploadAvatar user={user} close={closeModal} />
           </DialogHeader>
         </DialogContent>
       </Dialog>
--- features/profile-settings/components/upload-avatar.tsx
+++ features/profile-settings/components/upload-avatar.tsx
@@ -5,16 +5,20 @@ import "cropperjs/dist/cropper.css";
 import { ChangeEvent, useState } from "react";
 
 import Cropper from "react-cropper";
+import { Input } from "@/components/ui/input";
+
 import { PublicProfile } from "@/types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { upsertUserAvatar } from "../api/upsert-user-avatar";
 import { v4 as uuidv4 } from "uuid";
+import { Button } from "@/components/ui/button";
 
 type UploadAvatarProps = {
   user: PublicProfile;
+  close: () => void;
 };
 
-export function UploadAvatar({ user }: UploadAvatarProps) {
+export function UploadAvatar({ user, close }: UploadAvatarProps) {
   const [image, setImage] = useState<string | null>(null);
 
   const [cropper, setCropper] = useState<Cropper>();
@@ -59,6 +63,8 @@ export function UploadAvatar({ user }: UploadAvatarProps) {
                 biography: user.biography,
               })
               .select();
+
+            close();
           }
         }
         if (error) {
@@ -70,27 +76,40 @@ export function UploadAvatar({ user }: UploadAvatarProps) {
 
   return (
     <div>
-      <input
+      <div className="grid w-full max-w-sm items-center  gap-1.5">
+        <Input
+          className="h-10 p-0 mt-5"
+          id="picture"
+          type="file"
+          accept="image/png, image/jpeg, image/jpg"
+          onChange={setNewImage}
+        />
+      </div>
+      {/* <input
         type="file"
         accept="image/png, image/jpeg, image/jpg"
         onChange={setNewImage}
-      />
+      /> */}
       {image && (
         <>
-          <Cropper
-            aspectRatio={1}
-            src={image}
-            style={{ height: 400, width: 400 }}
-            initialAspectRatio={4 / 3}
-            minCropBoxHeight={100}
-            minCropBoxWidth={100}
-            guides={false}
-            checkOrientation={false}
-            onInitialized={(instance) => {
-              setCropper(instance);
-            }}
-          />
-          <button onClick={getCropData}>Crop Image</button>
+          <div className="flex justify-center max-w-full mt-5">
+            <Cropper
+              aspectRatio={1}
+              src={image}
+              style={{ height: 400, width: 400 }}
+              initialAspectRatio={4 / 4}
+              minCropBoxHeight={100}
+              minCropBoxWidth={100}
+              guides={false}
+              checkOrientation={false}
+              onInitialized={(instance) => {
+                setCropper(instance);
+              }}
+            />
+          </div>
+          <div className="flex justify-center mt-5">
+            <Button onClick={getCropData}>Save Avatar</Button>
+          </div>
         </>
       )}
     </div>
