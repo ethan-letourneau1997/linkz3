56f25c633c0a58ac64a3162675b7219b7ec02779
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-18 02:47:09 -0400

    Delete unused params

--- app/(playground)/playground/page.tsx
+++ app/(playground)/playground/page.tsx
@@ -2,12 +2,9 @@ import { Playground } from "./playground";
 
 export const dynamic = "force-dynamic";
 
-export default async function Index({ params, searchParams }) {
+export default async function Index() {
   return (
     <>
-      <div>
-        {searchParams.a} {searchParams.b}
-      </div>
       <Playground />
     </>
   );
