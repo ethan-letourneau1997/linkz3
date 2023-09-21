750f026007cb3623abee6f559e978d6d75d3c4fa
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-21 19:47:00 -0400

    Refactor carousel

--- features/image-carousel/components/image-carousel.tsx
+++ features/image-carousel/components/image-carousel.tsx
@@ -2,6 +2,7 @@
 
 import "keen-slider/keen-slider.min.css";
 
+import { CarouselArrow } from "./carousel-arrow";
 import Image from "next/image";
 import { PostImage } from "@/types";
 import { useKeenSlider } from "keen-slider/react";
@@ -46,7 +47,7 @@ export function ImageCarousel({ postImages }: ImageCarouselProps) {
           </div>
           {loaded && instanceRef.current && (
             <>
-              <Arrow
+              <CarouselArrow
                 left
                 onClick={(e: { stopPropagation: () => unknown }) =>
                   e.stopPropagation() || instanceRef.current?.prev()
@@ -54,7 +55,7 @@ export function ImageCarousel({ postImages }: ImageCarouselProps) {
                 disabled={currentSlide === 0}
               />
 
-              <Arrow
+              <CarouselArrow
                 onClick={(e: { stopPropagation: () => unknown }) =>
                   e.stopPropagation() || instanceRef.current?.next()
                 }
@@ -86,29 +87,4 @@ export function ImageCarousel({ postImages }: ImageCarouselProps) {
         )}
       </>
     );
-}
-
-function Arrow(props: {
-  disabled: boolean;
-  left?: boolean;
-  onClick: (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void;
-}) {
-  const disabeld = props.disabled ? " arrow--disabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? "arrow--left" : "arrow--right"
-      } ${disabeld}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
-  );
 }
