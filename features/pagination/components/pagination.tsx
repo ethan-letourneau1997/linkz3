be0daad571b7b6690f6a880eb086181e49a8361e
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-20 22:14:13 -0400

    Change pagination buttons to Link components

--- features/pagination/components/pagination.tsx
+++ features/pagination/components/pagination.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
-import { useCallback } from "react";
+import Link from "next/link";
 
 type PaginationProps = {
   currentPage: number;
@@ -12,110 +11,83 @@ type PaginationProps = {
 };
 
 export function Pagination({ totalPages, currentPage }: PaginationProps) {
-  const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  console.log(pathname);
 
-  const [optimisticPage, setOptimisticPage] =
-    useOptimistic<number>(currentPage);
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams);
-      params.set(name, value);
-
-      return params.toString();
-    },
-    [searchParams]
-  );
-
-  function goToPrevious() {
-    setOptimisticPage((prev) => prev - 1);
-    router.push(
-      pathname + "?" + createQueryString("page", `${currentPage - 1}`)
-    );
-  }
-  function goToNext() {
-    setOptimisticPage((prev) => prev + 1);
-
-    router.push(
-      pathname + "?" + createQueryString("page", `${currentPage + 1}`)
-    );
-  }
-
-  function goToNumber(number: number) {
-    setOptimisticPage(number);
-
-    router.push(pathname + "?" + createQueryString("page", `${number}`));
-  }
-
-  if (optimisticPage)
+  if (currentPage)
     return (
       <div className="flex gap-5 mt-5 dark:text-neutral-300">
-        {optimisticPage > 1 ? (
-          <button
-            onClick={goToPrevious}
+        {currentPage > 1 ? (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage - 1}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
             <FaChevronLeft />
-          </button>
+          </Link>
         ) : (
-          <button
-            disabled
-            className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:text-neutral-500"
-          >
+          <div className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:text-neutral-500">
             <FaChevronLeft />
-          </button>
+          </div>
         )}
-        {optimisticPage - 2 > 0 && (
-          <button
-            onClick={() => goToNumber(optimisticPage - 2)}
+        {currentPage - 2 > 0 && (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage - 2}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
-            {optimisticPage - 2}
-          </button>
+            {currentPage - 2}
+          </Link>
         )}
-        {optimisticPage - 1 > 0 && (
-          <button
-            onClick={() => goToNumber(optimisticPage - 1)}
+        {currentPage - 1 > 0 && (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage - 1}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
-            {optimisticPage - 1}
-          </button>
+            {currentPage - 1}
+          </Link>
         )}
         <div className="flex items-center justify-center w-10 h-10 text-xl font-bold rounded dark:bg-indigo-600">
-          {optimisticPage}
+          {currentPage}
         </div>
-        {optimisticPage + 1 <= totalPages && (
-          <button
-            onClick={() => goToNumber(optimisticPage + 1)}
+        {currentPage + 1 <= totalPages && (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage + 1}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
-            {optimisticPage + 1}
-          </button>
+            {currentPage + 1}
+          </Link>
         )}
-        {optimisticPage + 2 <= totalPages && (
-          <button
-            onClick={() => goToNumber(optimisticPage + 2)}
+        {currentPage + 2 <= totalPages && (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage + 2}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
-            {optimisticPage + 2}
-          </button>
+            {currentPage + 2}
+          </Link>
         )}
-        {optimisticPage < totalPages ? (
-          <button
-            onClick={goToNext}
+        {currentPage < totalPages ? (
+          <Link
             className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500"
+            href={`${pathname}?page=${currentPage + 1}&sort=${searchParams.get(
+              "sort"
+            )}`}
           >
             <FaChevronRight />
-          </button>
+          </Link>
         ) : (
-          <button
-            disabled
-            className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500 dark:text-neutral-500"
-          >
+          <div className="flex items-center justify-center w-10 h-10 text-xl font-bold dark:hover:text-indigo-500 dark:text-neutral-500">
             <FaChevronRight />
-          </button>
+          </div>
         )}
       </div>
     );
