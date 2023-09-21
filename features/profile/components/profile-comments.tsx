58cef8891e1fbbf4f5dccc633bc39f43b738f9e5
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-20 20:27:10 -0400

    Add fallbacks for empty profile posts and comments arrays

--- features/profile/components/profile-comments.tsx
+++ features/profile/components/profile-comments.tsx
@@ -21,15 +21,23 @@ export async function ProfileComments({
     searchParams.page
   );
 
-  return (
-    <div className="w-full max-w-3xl my-5 space-y-5">
-      {comments?.map((comment) => (
-        <CommentPreview
-          key={comment.id}
-          comment={comment}
-          username={params.username}
-        />
-      ))}
-    </div>
-  );
+  if (comments && comments.length > 0)
+    return (
+      <div className="w-full max-w-3xl my-5 space-y-5">
+        {comments?.map((comment) => (
+          <CommentPreview
+            key={comment.id}
+            comment={comment}
+            username={params.username}
+          />
+        ))}
+      </div>
+    );
+
+  if (comments && comments.length === 0)
+    return (
+      <div className="mt-10 text-center text-neutral-300">
+        {params.username} has not made any comments.
+      </div>
+    );
 }
