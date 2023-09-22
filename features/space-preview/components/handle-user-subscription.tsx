49db5c63e3538cf7d505bd263d41a99ceebb84dd
ethan-letourneau1997 <ethan.michael.letourneau@gmail.com>
2023-09-21 22:30:57 -0400

    Refactor space preview

--- features/space-preview/components/handle-user-subscription.tsx
+++ features/space-preview/components/handle-user-subscription.tsx
@@ -1,41 +1,13 @@
 import { Space } from "@/types";
 import { SubscribeButton } from "./subscribe-button";
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { checkUserSubscription } from "@/lib/space/check-user-subscription";
 
 type UserSubscriptionProps = {
   space: Space;
 };
 
 export async function HandleUserSubscription({ space }: UserSubscriptionProps) {
-  const supabase = createServerComponentClient({ cookies });
+  const isSubscribed = await checkUserSubscription(space.id);
 
-  const { data } = await supabase.auth.getSession();
-
-  async function checkUserSubscription() {
-    if (data.session) {
-      try {
-        const { data: user_community } = await supabase
-          .from("user_community")
-          .select()
-          .match({ user_id: data.session.user.id, community_id: space.id });
-
-        if (user_community && user_community.length > 0) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        (error);
-      }
-    }
-  }
-
-  const isSubscribed = await checkUserSubscription();
-
-  return (
-    <>
-      <SubscribeButton space={space} isSubscribed={isSubscribed || false} />
-    </>
-  );
+  return <SubscribeButton space={space} isSubscribed={isSubscribed || false} />;
 }
